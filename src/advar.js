fc2bacacc19dec99e2ce835aa2b280d62ba3e15b
Paulo Cândido <pcandido.m@gmail.com>
2020-02-17 22:05:33 -0300

    UC2 - provide several ways to build an advar

--- src/advar.js
+++ src/advar.js
@@ -3,6 +3,33 @@ class advar{
   _data;
 
   /**
+   * Constructor of advar. It can be called in a several ways:
+   * 1) with no param for an empty array
+   * 2) with a single non-array-like element for an array of one element
+   * 3) with a single array-like element for a copy of this array
+   * 4) with a single advar object for a copy of this advar
+   * 5) with multiple params for a multi-element array
+   *
+   * @param data is the elements to build an advar
+   */
+  constructor(...data){
+    if(data.length === 0){
+      this._data = [];
+    } else if(data.length === 1) {
+      data = data[0];
+      if (Array.isArray(data)){
+        this._data = [...data];
+      } else if(data instanceof advar) {
+        this._data = [...data._data];
+      } else {
+        this._data = [data];
+      }
+    } else {
+      this._data = data;
+    }
+  }
+
+  /**
    * Returns the raw data as a native array
    */
   toArray(){
--- test/advar.js
+++ test/advar.js
@@ -1,6 +1,8 @@
 const assert = require('assert');
 const { advar } = require('../src/advar');
 
+const de = assert.deepEqual;
+
 describe('advar', () => {
 
   describe('toArray', () => {
@@ -8,8 +10,64 @@ describe('advar', () => {
     it('should return the raw stored data', () => {
       const a = new advar();
       a._data = [1, 2, 3];  //hard-coded to assert toArray without depends to the constructor
-      assert.deepEqual(a.toArray(), [1, 2, 3]);
+      de(a.toArray(), [1, 2, 3]);
     });
+
+  });
+
+  describe('constructor', () => {
+
+    it('should be called with no parameters', () => {
+      de(new advar().toArray(), []);
+    });
+
+    it('should accpet a single non-array-like element', () => {
+      de(new advar(1).toArray(), [1]);
+      de(new advar('a').toArray(), ['a']);
+      de(new advar({a:1}).toArray(), [{a:1}]);
+      de(new advar(true).toArray(), [true]);
+    });
+
+    it('should accept a single array-like element', () => {
+      de(new advar([]).toArray(), []);
+      de(new advar([1]).toArray(), [1]);
+      de(new advar(['a']).toArray(), ['a']);
+      de(new advar([{a:1}]).toArray(), [{a:1}]);
+      de(new advar([1,2,3]).toArray(), [1,2,3]);
+      de(new advar(['a',1,false]).toArray(), ['a',1,false]);
+    });
+
+    it('should create a copy of array instead of a reference', () => {
+      let array = [1,2,3];
+      let adv = new advar(array);
+      array.pop();
+      de(array, [1,2]);
+      de(adv.toArray(), [1,2,3]);
+
+      //TODO: make a change on adv and check array
+    });
+
+    it('should accept a single advar to be cloned', () => {
+      de(new advar(new advar()).toArray(), []);
+      de(new advar(new advar([1,2,3])).toArray(), [1,2,3]);
+    });
+
+    it('should create a copy of advar instead of a reference', () => {
+      let adv1 = new advar([1,2,3]);
+      let adv2 = new advar(adv1);
+      
+      //TODO: make a change on adv1 and check adv2 and vice-versa
+    });
+
+    it('should accpet multiple elements', () => {
+      de(new advar(1,2,3).toArray(), [1,2,3]);
+      de(new advar([1],2,'3',{a:4}).toArray(), [[1],2,'3',{a:4}]);
+      de(new advar([],[]).toArray(), [[],[]]);
+    });
+
+    
+
+    
 
   });
 
