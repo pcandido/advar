d871aa87e6278afdf61bfb69a992b58b73fcc9f5
Paulo Cândido <pcandido.m@gmail.com>
2020-02-17 21:23:02 -0300

    UC1 - returns the raw data as a native array

--- src/advar.js
+++ src/advar.js
@@ -1,3 +1,14 @@
 class advar{
 
+  _data;
+
+  /**
+   * Returns the raw data as a native array
+   */
+  toArray(){
+    return this._data;
+  }
+
 }
+
+module.exports = { advar }
--- test/advar.js
+++ test/advar.js
@@ -1,2 +1,16 @@
 const assert = require('assert');
-const advar = require('../src/advar');
+const { advar } = require('../src/advar');
+
+describe('advar', () => {
+
+  describe('toArray', () => {
+
+    it('should return the raw stored data', () => {
+      const a = new advar();
+      a._data = [1, 2, 3];  //hard-coded to assert toArray without depends to the constructor
+      assert.deepEqual(a.toArray(), [1, 2, 3]);
+    });
+
+  });
+
+});
