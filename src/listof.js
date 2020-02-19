b45cb20acd4695b844a57cb8406554167f9c562c
Paulo Cândido <pcandido.m@gmail.com>
2020-02-19 00:04:12 -0300

    rename completo

--- src/listof.js
+++ src/listof.js
@@ -1,6 +1,6 @@
 const util = require('util');
 
-class List{
+class List {
 
   _data;
 
@@ -9,8 +9,8 @@ class List{
    *
    * @param data an array with the elements
    */
-  constructor(data){
-    if(!Array.isArray(data))
+  constructor(data) {
+    if (!Array.isArray(data))
       throw new Error('Invalid data input');
 
     this._data = data;
@@ -19,24 +19,24 @@ class List{
   /**
    * Returns the raw data as a native array
    */
-  toArray(){
+  toArray() {
     return this._data;
   }
 
   /**
-   * An node debugger/inspect representation for the advar. It will be the same of the raw data (array-like representation).
+   * An node debugger/inspect representation for the list. It will be the same of the raw data (array-like representation).
    */
   [util.inspect.custom](depth, opts) {
     return this._data;
   }
 
   /**
-   * Gets an element from advar by index. Negative indices will get elements backwards. Out of bound indices will return undefined.
+   * Gets an element from list by index. Negative indices will get elements backwards. Out of bound indices will return undefined.
    *
    * @param index is the position from where the element should be gotten
    */
-  get(index){
-    if(index < 0)
+  get(index) {
+    if (index < 0)
       index = this._data.length + index;
 
     return this._data[index];
@@ -44,4 +44,62 @@ class List{
 
 }
 
-exports.array = (data) => new List(data);
+/**
+   * Builds a list from several ways:
+   * 1) with no param for an empty array
+   * 2) with a single non-array-like element for an array of one element
+   * 3) with a single array-like element for a copy of this array
+   * 4) with a single list object for a copy of this list
+   * 5) with multiple params for a multi-element array
+   *
+   * @param data is the elements to build an list
+   */
+exports.of = (...data) => {
+  if (data.length === 0) {
+    return new List([]);
+  } else if (data.length === 1) {
+    data = data[0];
+    if (Array.isArray(data)) {
+      return new List([...data]);
+    } else if (data instanceof List) {
+      return new List([...data._data]);
+    } else {
+      return new List([data]);
+    }
+  } else {
+    return new List(data);
+  }
+}
+
+/**
+ * Builds a list from a range. It can be used on several ways:
+ * 1) with one param, the range iterates 1-by-1 from 0 (inclusive) to the value of param (exclusive)
+ * 2) with two params, the range iterates 1-by-1 from the first param (inclusive) to the second param (exclusive)
+ * 3) with three params, the range iterates by the value of the third param, from the value of the first param (inclusive) to the value of the second param (exclusive)
+ */
+exports.range = (start, end, step) => {
+  if (typeof start === 'undefined') {
+    throw new Error('No param was supplied');
+  }
+
+  if (typeof end === 'undefined') {
+    end = start;
+    start = 0;
+  }
+
+  if (typeof step === 'undefined') {
+    step = end < start ? -1 : 1;
+  }
+
+  if ((end < start && step >= 0) || (start < end && step <= 0)) {
+    throw new Error(`Invalid step ${step}`);
+  }
+
+  data = [];
+  if (end < start) {
+    for (let i = start; i > end; i += step) data.push(i);
+  } else {
+    for (let i = start; i < end; i += step) data.push(i);
+  }
+  return new List(data);
+}
--- test/listof.js
+++ test/listof.js
@@ -7,6 +7,72 @@ const seq = assert.strictEqual;
 
 describe('listof', () => {
 
+  describe('of', () => {
+
+    it('should be called with no parameters', () => {
+      deq(listof.of().toArray(), []);
+    });
+
+    it('should accpet a single non-array-like element', () => {
+      deq(listof.of(1).toArray(), [1]);
+      deq(listof.of('a').toArray(), ['a']);
+      deq(listof.of({ a: 1 }).toArray(), [{ a: 1 }]);
+      deq(listof.of(true).toArray(), [true]);
+    });
+
+    it('should accept a single array-like element', () => {
+      deq(listof.of([]).toArray(), []);
+      deq(listof.of([1]).toArray(), [1]);
+      deq(listof.of(['a']).toArray(), ['a']);
+      deq(listof.of([{ a: 1 }]).toArray(), [{ a: 1 }]);
+      deq(listof.of([1, 2, 3]).toArray(), [1, 2, 3]);
+      deq(listof.of(['a', 1, false]).toArray(), ['a', 1, false]);
+    });
+
+    it('should accept a single list to be cloned', () => {
+      deq(listof.of(listof.of()).toArray(), []);
+      deq(listof.of(listof.of([1, 2, 3])).toArray(), [1, 2, 3]);
+    });
+
+    it('should accpet multiple elements', () => {
+      deq(listof.of(1, 2, 3).toArray(), [1, 2, 3]);
+      deq(listof.of([1], 2, '3', { a: 4 }).toArray(), [[1], 2, '3', { a: 4 }]);
+      deq(listof.of([], []).toArray(), [[], []]);
+    });
+
+  });
+
+  describe('range', () => {
+
+    it('should throws an exception if no param is provided', () => {
+      assert.throws(() => { listof.range() }, Error);
+    })
+
+    it('should build a list with 1 param', () => {
+      deq(listof.range(0).toArray(), []);
+      deq(listof.range(3).toArray(), [0, 1, 2]);
+      deq(listof.range(-3).toArray(), [0, -1, -2]);
+    });
+
+    it('should build a list with 2 params', () => {
+      deq(listof.range(3, 3).toArray(), []);
+      deq(listof.range(0, 3).toArray(), [0, 1, 2]);
+      deq(listof.range(3, 6).toArray(), [3, 4, 5]);
+      deq(listof.range(-3, 3).toArray(), [-3, -2, -1, 0, 1, 2]);
+      deq(listof.range(3, -3).toArray(), [3, 2, 1, 0, -1, -2]);
+    });
+
+    it('should build a list with 3 params', () => {
+      deq(listof.range(5, 10, 1).toArray(), [5, 6, 7, 8, 9]);
+      deq(listof.range(5, 10, 2).toArray(), [5, 7, 9]);
+      deq(listof.range(5, 11, 2).toArray(), [5, 7, 9]);
+      deq(listof.range(11, 5, -2).toArray(), [11, 9, 7]);
+      assert.throws(() => { listof.range(5, 10, -1) }, Error);
+      assert.throws(() => { listof.range(10, 5, 1) }, Error);
+    });
+
+  });
+
 });
 
 describe('List', () => {
@@ -14,82 +80,30 @@ describe('List', () => {
   describe('toArray', () => {
 
     it('should return the raw stored data', () => {
-      const a = listof.array();
+      const a = listof.of();
       a._data = [1, 2, 3];  //hard-coded to assert toArray without depends to the constructor
-      de(a.toArray(), [1, 2, 3]);
+      deq(a.toArray(), [1, 2, 3]);
     });
 
-  });
-
-  describe('constructor', () => {
-
-    it('should be called with no parameters', () => {
-      de(new advar().toArray(), []);
-    });
-
-    it('should accpet a single non-array-like element', () => {
-      de(new advar(1).toArray(), [1]);
-      de(new advar('a').toArray(), ['a']);
-      de(new advar({a:1}).toArray(), [{a:1}]);
-      de(new advar(true).toArray(), [true]);
-    });
-
-    it('should accept a single array-like element', () => {
-      de(new advar([]).toArray(), []);
-      de(new advar([1]).toArray(), [1]);
-      de(new advar(['a']).toArray(), ['a']);
-      de(new advar([{a:1}]).toArray(), [{a:1}]);
-      de(new advar([1,2,3]).toArray(), [1,2,3]);
-      de(new advar(['a',1,false]).toArray(), ['a',1,false]);
-    });
-
-    it('should create a copy of array instead of a reference', () => {
-      let array = [1,2,3];
-      let adv = new advar(array);
-      array.pop();
-      de(array, [1,2]);
-      de(adv.toArray(), [1,2,3]);
-
-      //TODO: make a change on adv and check array
-    });
-
-    it('should accept a single advar to be cloned', () => {
-      de(new advar(new advar()).toArray(), []);
-      de(new advar(new advar([1,2,3])).toArray(), [1,2,3]);
-    });
-
-    it('should create a copy of advar instead of a reference', () => {
-      let adv1 = new advar([1,2,3]);
-      let adv2 = new advar(adv1);
-      
-      //TODO: make a change on adv1 and check adv2 and vice-versa
-    });
-
-    it('should accpet multiple elements', () => {
-      de(new advar(1,2,3).toArray(), [1,2,3]);
-      de(new advar([1],2,'3',{a:4}).toArray(), [[1],2,'3',{a:4}]);
-      de(new advar([],[]).toArray(), [[],[]]);
-    });
-  
   });
 
   describe('get', () => {
 
     it('should return an element by index', () => {
-	const ad = new advar(1,2,3);
-	eq(ad.get(1), 2);
-        eq(ad.get(0), 1);
+      const ad = listof.of(1, 2, 3);
+      eq(ad.get(1), 2);
+      eq(ad.get(0), 1);
     });
 
     it('should return elements backwards for negative indices', () => {
-        const ad = new advar(1,2,3);
-	eq(ad.get(-1), 3);
-        eq(ad.get(-2), 2);
+      const ad = listof.of(1, 2, 3);
+      eq(ad.get(-1), 3);
+      eq(ad.get(-2), 2);
     });
 
     it('should return undefined for out of bound indices', () => {
-      seq(new advar(1,2,3).get(5), undefined);
-      seq(new advar(1,2,3).get(-5), undefined);
+      seq(listof.of(1, 2, 3).get(5), undefined);
+      seq(listof.of(1, 2, 3).get(-5), undefined);
     });
   });
 
